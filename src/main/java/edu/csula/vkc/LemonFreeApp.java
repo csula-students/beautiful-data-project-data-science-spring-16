5f7483ca83049995232c9f03fb875cf663e08723
Kuntal Naphade <kuntal.s.naphade@gmail.com>
2016-04-23 14:42:35 -0800

    App and Service with updated methods

--- src/main/java/edu/csula/vkc/LemonFreeApp.java
+++ src/main/java/edu/csula/vkc/LemonFreeApp.java
@@ -1,5 +1,41 @@
 package edu.csula.vkc;
 
-public class LemonFreeApp {
+import com.mashape.unirest.http.JsonNode;
 
+public class LemonFreeApp {
+	public static void main(String[] args) {
+
+		JsonNode carMakes = LemonFreeService.getMakes();
+
+		System.out.println("getMakes response-->" + carMakes);
+
+		JsonNode carModelsByMakeID = LemonFreeService.getModelsByMakeID();
+
+		System.out.println("getModelsByMakeID response-->" + carModelsByMakeID);
+
+		JsonNode carTrimsByMakeandModel = LemonFreeService.getTrimsByMakeandModel();
+
+		System.out.println("getTrimsByMakeandModel response-->" + carTrimsByMakeandModel);
+
+		JsonNode carYearsByMakeandModel = LemonFreeService.getYearsByMakeandModel();
+
+		System.out.println("getYearsByMakeandModel response-->" + carYearsByMakeandModel);
+
+		JsonNode carPricesByMake = LemonFreeService.getPricesByMake();
+
+		System.out.println("getPricesByMake response-->" + carPricesByMake);
+
+		JsonNode carLocations = LemonFreeService.getLocations();
+
+		System.out.println("getLocations response-->" + carLocations);
+
+		JsonNode carLocationsbyMake = LemonFreeService.getLocationsbyMake();
+
+		System.out.println("getLocationsbyMake response-->" + carLocationsbyMake);
+
+		JsonNode carListingsbyMakeAndModel = LemonFreeService.getListingsbyMakeAndModel();
+
+		System.out.println("getListingsbyMakeAndModel response-->" + carListingsbyMakeAndModel);
+
+	}
 }
--- src/main/java/edu/csula/vkc/LemonFreeService.java
+++ src/main/java/edu/csula/vkc/LemonFreeService.java
@@ -1,5 +1,129 @@
 package edu.csula.vkc;
 
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class LemonFreeService {
+
+	public static JsonNode getMakes() {
+
+		JsonNode responseMake = null;
+		try {
+			responseMake = Unirest
+					.get("http://api.lemonfree.com/v2/makes/?format=json&key=07859aa5804ac8c1425bdb703ca961ed").asJson()
+					.getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseMake;
+	}
+
+	public static JsonNode getModelsByMakeID() {
+
+		JsonNode responseModelsByMakeID = null;
+		try {
+			responseModelsByMakeID = Unirest
+					.get("http://api.lemonfree.com/v2/models/?make_id=11&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseModelsByMakeID;
+	}
+
+	public static JsonNode getTrimsByMakeandModel() {
+
+		JsonNode responseTrimsByMakeandModel = null;
+		try {
+			responseTrimsByMakeandModel = Unirest
+					.get("http://api.lemonfree.com/v2/trims/?make=Chevrolet&model=Cavalier&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseTrimsByMakeandModel;
+	}
+
+	public static JsonNode getYearsByMakeandModel() {
+
+		JsonNode responseYearsByMakeandModel = null;
+		try {
+			responseYearsByMakeandModel = Unirest
+					.get("http://api.lemonfree.com/v2/years/?make=Nissan&model=Altima&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseYearsByMakeandModel;
+	}
+
+	public static JsonNode getPricesByMake() {
+
+		JsonNode responsePricesByMake = null;
+		try {
+			responsePricesByMake = Unirest
+					.get("http://api.lemonfree.com/v2/prices/?make=Oldsmobile&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responsePricesByMake;
+	}
+
+	public static JsonNode getLocations() {
+
+		JsonNode responseLocations = null;
+		try {
+			responseLocations = Unirest
+					.get("http://api.lemonfree.com/v2/locations/?format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseLocations;
+	}
+
+	public static JsonNode getLocationsbyMake() {
+
+		JsonNode responseLocationsbyMake = null;
+		try {
+			responseLocationsbyMake = Unirest
+					.get("http://api.lemonfree.com/v2/locations/?make=Toyota&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseLocationsbyMake;
+	}
+
+	public static JsonNode getListingsbyMakeAndModel() {
+
+		JsonNode responseListingsbyMakeAndModel = null;
+		try {
+			responseListingsbyMakeAndModel = Unirest
+					.get("	http://api.lemonfree.com/v2/listings/?make=Toyota&model=Camry&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.asJson().getBody();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return responseListingsbyMakeAndModel;
+	}
 
 }
