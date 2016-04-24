817f1e77dff43cf8310bb44ad4b8f248bc6b2a0e
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 16:52:14 -0700

    Added Dummy Source for Test of Homework 2.

---
+++ src/main/java/edu/csula/vkc/util/DummySource.java
@@ -0,0 +1,22 @@
+package edu.csula.vkc.util;
+
+import java.util.Collection;
+
+import edu.csula.datascience.acquisition.Source;
+import edu.csula.vkc.models.Make;
+
+public class DummySource implements Source<Make>{
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Make> next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
