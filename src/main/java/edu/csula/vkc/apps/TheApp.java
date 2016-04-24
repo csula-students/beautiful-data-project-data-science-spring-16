ee09b0eb7599e1dfe493c6c6e81fd649a89334c0
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 16:48:59 -0700

    Updates to The App, Deleted useless files.

--- src/main/java/edu/csula/vkc/apps/TheApp.java
+++ src/main/java/edu/csula/vkc/apps/TheApp.java
@@ -2,21 +2,35 @@ package edu.csula.vkc.apps;
 
 import java.util.Collection;
 import edu.csula.vkc.models.Make;
+import edu.csula.vkc.util.CSVSource;
 import edu.csula.vkc.util.EdmundsSource;
 import edu.csula.vkc.util.GenericCollector;
 import edu.csula.vkc.util.LemonSource;
 
 public class TheApp {
 	public static void main(String[] args) throws Exception {
+
+		//Sources
+		EdmundsSource edmundsSource = new EdmundsSource();
+		LemonSource lemonSource = new LemonSource();
+		CSVSource csvSource = new CSVSource();
 		
-        LemonSource source = new LemonSource();
-        GenericCollector collector = new GenericCollector();        
- 
-        if(source.hasNext()){
-            Collection<Make> carMetadata = source.getData();
-            //System.out.println("Hello 1");
-            //Collection<Make> cleanedTweets = collector.mungee(carMetadata);
-            //collector.save(cleanedTweets);
-        }
-    }
+		GenericCollector collector = new GenericCollector();
+
+		Collection<Make> edmundsData = edmundsSource.getData();
+		Collection<Make> cleanedEdmundsData = collector.mungee(edmundsData);
+		collector.save(cleanedEdmundsData);
+
+		Collection<Make> lemonData = lemonSource.getData();
+		Collection<Make> cleanedLemonData = collector.mungee(lemonData);
+		collector.save(cleanedLemonData);
+		
+		if(csvSource.hasNext()){
+			Collection<Make> csvData = csvSource.next();
+			Collection<Make> cleanedCSVData = collector.mungee(csvData);
+			collector.save(cleanedCSVData);
+		}
+		
+		
+	}
 }
