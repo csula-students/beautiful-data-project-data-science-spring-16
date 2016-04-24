6e9321d164dcf2310cfe92ea449903e964aeab58
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 02:35:26 -0700

    Push Styles.

--- src/main/java/edu/csula/vkc/models/Styles.java
+++ src/main/java/edu/csula/vkc/models/Styles.java
@@ -3,8 +3,16 @@ package edu.csula.vkc.models;
 public class Styles {
 	
 	private long styleId;
-	private int numofDoors;
-	
+	private String name;
+	private int numOfDoors;
+	private MPG mpg;
+	private String vehicalType;
+	private int noOfCylinder;
+	private String engineLocation;
+	private String transmission;
+	private String driveSystem;
+	private String fuelType;
+	private String trim;
 	
 
 }
