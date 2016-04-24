37e039c9f9317bc887f9d678bb97d71fa5138e53
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 00:46:59 -0700

    update to model.

--- src/main/java/edu/csula/vkc/models/Model.java
+++ src/main/java/edu/csula/vkc/models/Model.java
@@ -4,33 +4,18 @@ import java.util.List;
 
 public class Model {
 	
-	long model_id;
-	String model;
-	String vehicle_type;
-	int no_of_Cylinder;
-	String engine_location;
-	String transmission;
-	String drive_system;
-	String fuel_type;
-	long style_id;
-	List<Years> year;
+	private long model_id;
+	private String model;
+	private List<Years> year;
 	
 	public Model() {
 		super();
 	}
 
-	public Model(long model_id, String model, String vehicle_type, int no_of_Cylinder, String engine_location,
-			String transmission, String drive_system, String fuel_type, long style_id, List<Years> year) {
+	public Model(long model_id, String model, List<Years> year) {
 		super();
 		this.model_id = model_id;
 		this.model = model;
-		this.vehicle_type = vehicle_type;
-		this.no_of_Cylinder = no_of_Cylinder;
-		this.engine_location = engine_location;
-		this.transmission = transmission;
-		this.drive_system = drive_system;
-		this.fuel_type = fuel_type;
-		this.style_id = style_id;
 		this.year = year;
 	}
 
@@ -50,63 +35,6 @@ public class Model {
 		this.model = model;
 	}
 
-	public String getVehicle_type() {
-		return vehicle_type;
-	}
-
-	public void setVehicle_type(String vehicle_type) {
-		this.vehicle_type = vehicle_type;
-	}
-
-	public int getNo_of_Cylinder() {
-		return no_of_Cylinder;
-	}
-
-	public void setNo_of_Cylinder(int no_of_Cylinder) {
-		this.no_of_Cylinder = no_of_Cylinder;
-	}
-
-	public String getEngine_location() {
-		return engine_location;
-	}
-
-	public void setEngine_location(String engine_location) {
-		this.engine_location = engine_location;
-	}
-
-	public String getTransmission() {
-		return transmission;
-	}
-
-	public void setTransmission(String transmission) {
-		this.transmission = transmission;
-	}
-
-	public String getDrive_system() {
-		return drive_system;
-	}
-
-	public void setDrive_system(String drive_system) {
-		this.drive_system = drive_system;
-	}
-
-	public String getFuel_type() {
-		return fuel_type;
-	}
-
-	public void setFuel_type(String fuel_type) {
-		this.fuel_type = fuel_type;
-	}
-
-	public long getStyle_id() {
-		return style_id;
-	}
-
-	public void setStyle_id(long style_id) {
-		this.style_id = style_id;
-	}
-
-
 	public List<Years> getYear() {
 		return year;
 	}
@@ -114,5 +42,4 @@ public class Model {
 	public void setYear(List<Years> year) {
 		this.year = year;
 	}
-	
 }
