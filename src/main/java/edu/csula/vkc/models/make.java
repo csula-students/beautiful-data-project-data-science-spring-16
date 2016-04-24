e14370b7caa8686dd0415366f4adb0635b76915a
vidushidikshit <vibhashiva92@gmail.com>
2016-04-23 19:47:48 -0700

    changes to model

--- src/main/java/edu/csula/vkc/models/make.java
+++ src/main/java/edu/csula/vkc/models/make.java
@@ -1,40 +1,87 @@
 package edu.csula.vkc.models;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
 public class make {
-int make_id;
-String make;
-List<model> modelList= new ArrayList<>();
-public make(){
-	super();
-}
-public make(int make_id, String make, List<model> modelList) {
-	super();
-	this.make_id = make_id;
-	this.make = make;
-	this.modelList = modelList;
-}
-public int getMake_id() {
-	return make_id;
-}
-public void setMake_id(int make_id) {
-	this.make_id = make_id;
-}
-public String getMake() {
-	return make;
-}
-public void setMake(String make) {
-	this.make = make;
-}
-public List<model> getModelList() {
-	return modelList;
-}
-public void setModelList(List<model> modelList) {
-	this.modelList = modelList;
-}
+	long vehicle_id;
+	long make_id;
+	String make;
+	String source;
+	List<Year> year;
+	List<model> modelList;
+	
+	public make() {
+		super();
+	}
+
+	
+	public make(long vehicle_id, long make_id, String make, String source, List<Year> year, List<model> modelList,
+			long price) {
+		super();
+		this.vehicle_id = vehicle_id;
+		this.make_id = make_id;
+		this.make = make;
+		this.source = source;
+		this.year = year;
+		this.modelList = modelList;
+	}
 
 
+
+	public List<Year> getYear() {
+		return year;
+	}
+
+
+
+	public void setYear(List<Year> year) {
+		this.year = year;
+	}
+
+	public long getVehicle_id() {
+		return vehicle_id;
+	}
+
+	public void setVehicle_id(long vehicle_id) {
+		this.vehicle_id = vehicle_id;
+	}
+
+	public long getMake_id() {
+		return make_id;
+	}
+
+	public void setMake_id(long make_id) {
+		this.make_id = make_id;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public void setMake_id(int make_id) {
+		this.make_id = make_id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public List<model> getModelList() {
+		return modelList;
+	}
+
+	public void setModelList(List<model> modelList) {
+		this.modelList = modelList;
+	}
 
 }
--- src/main/java/edu/csula/vkc/models/model.java
+++ src/main/java/edu/csula/vkc/models/model.java
@@ -4,41 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class model {
-int model_id;
-String model;
-List<trim> trimList = new ArrayList<>();
+	int model_id;
+	String model;
+	String vehicle_type;
+	int no_of_Cylinder;
+	String transmission;
+	String drive_system;
+	double price;
+	List<trim> trimList = new ArrayList<>();
 
-public model(){
-	super();
-}
+	public model() {
+		super();
+	}
 
+	public model(int model_id, String model, List<trim> trimList) {
+		super();
+		this.model_id = model_id;
+		this.model = model;
+		this.trimList = trimList;
+	}
 
-public model(int model_id, String model, List<trim> trimList) {
-	super();
-	this.model_id = model_id;
-	this.model = model;
-	this.trimList = trimList;
-}
-public int getModel_id() {
-	return model_id;
-}
-public void setModel_id(int model_id) {
-	this.model_id = model_id;
-}
-public String getModel() {
-	return model;
-}
-public void setModel(String model) {
-	this.model = model;
-}
-public List<trim> getTrimList() {
-	return trimList;
-}
-public void setTrimList(List<trim> trimList) {
-	this.trimList = trimList;
-}
+	public int getModel_id() {
+		return model_id;
+	}
 
+	public void setModel_id(int model_id) {
+		this.model_id = model_id;
+	}
 
+	public String getModel() {
+		return model;
+	}
 
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public List<trim> getTrimList() {
+		return trimList;
+	}
+
+	public void setTrimList(List<trim> trimList) {
+		this.trimList = trimList;
+	}
 
 }
