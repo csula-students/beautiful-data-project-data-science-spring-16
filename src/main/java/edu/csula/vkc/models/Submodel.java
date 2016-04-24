d2c54e22b442708723c95b4a4c55a4eda6549d6a
Kuntal Naphade <kuntal.s.naphade@gmail.com>
2016-04-24 02:24:32 -0800

    submodel class

--- src/main/java/edu/csula/vkc/models/Submodel.java
+++ src/main/java/edu/csula/vkc/models/Submodel.java
@@ -2,4 +2,34 @@ package edu.csula.vkc.models;
 
 public class Submodel {
 
+	private long subModel_id;
+	private String modelName;
+
+	public Submodel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Submodel(long subModel_id, String modelName) {
+		super();
+		this.subModel_id = subModel_id;
+		this.modelName = modelName;
+	}
+
+	public long getSubModel_id() {
+		return subModel_id;
+	}
+
+	public void setSubModel_id(long subModel_id) {
+		this.subModel_id = subModel_id;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
 }
