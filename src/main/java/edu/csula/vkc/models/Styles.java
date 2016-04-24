15ac92b46745854847d9e1117f0ed48306e9e481
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 02:43:09 -0700

    Styles uPdate.

--- src/main/java/edu/csula/vkc/models/Styles.java
+++ src/main/java/edu/csula/vkc/models/Styles.java
@@ -1,7 +1,7 @@
 package edu.csula.vkc.models;
 
 public class Styles {
-	
+
 	private long styleId;
 	private String name;
 	private int numOfDoors;
@@ -13,6 +13,93 @@ public class Styles {
 	private String driveSystem;
 	private String fuelType;
 	private String trim;
-	
+
+	public long getStyleId() {
+		return styleId;
+	}
+
+	public void setStyleId(long styleId) {
+		this.styleId = styleId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumOfDoors() {
+		return numOfDoors;
+	}
+
+	public void setNumOfDoors(int numOfDoors) {
+		this.numOfDoors = numOfDoors;
+	}
+
+	public MPG getMpg() {
+		return mpg;
+	}
+
+	public void setMpg(MPG mpg) {
+		this.mpg = mpg;
+	}
+
+	public String getVehicalType() {
+		return vehicalType;
+	}
+
+	public void setVehicalType(String vehicalType) {
+		this.vehicalType = vehicalType;
+	}
+
+	public int getNoOfCylinder() {
+		return noOfCylinder;
+	}
+
+	public void setNoOfCylinder(int noOfCylinder) {
+		this.noOfCylinder = noOfCylinder;
+	}
+
+	public String getEngineLocation() {
+		return engineLocation;
+	}
+
+	public void setEngineLocation(String engineLocation) {
+		this.engineLocation = engineLocation;
+	}
+
+	public String getTransmission() {
+		return transmission;
+	}
+
+	public void setTransmission(String transmission) {
+		this.transmission = transmission;
+	}
+
+	public String getDriveSystem() {
+		return driveSystem;
+	}
+
+	public void setDriveSystem(String driveSystem) {
+		this.driveSystem = driveSystem;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public String getTrim() {
+		return trim;
+	}
+
+	public void setTrim(String trim) {
+		this.trim = trim;
+	}
 
 }
