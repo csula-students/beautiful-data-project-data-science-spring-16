eba673240b6ab09538325bc6297a683b1f1a5e61
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-24 19:25:35 -0700

    LemonFree final Data Excractor with 2 key rotation.

--- src/main/java/edu/csula/vkc/services/LemonFreeService.java
+++ src/main/java/edu/csula/vkc/services/LemonFreeService.java
@@ -5,13 +5,19 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class LemonFreeService {
+	
+	static String keyKuntal = "07859aa5804ac8c1425bdb703ca961ed";
+	static String keyChitta = "93fa1b00fac63ed8fc0317294736f57f";
+	//static String keyVidus = "gfjuzyug6ajpxeejf9tdse48";
+
+	static int round = 0;
 
 	public JsonNode getMakes() {
 
 		JsonNode responseMake = null;
 		try {
 			responseMake = Unirest
-					.get("http://api.lemonfree.com/v2/makes/?format=json&key=07859aa5804ac8c1425bdb703ca961ed").asJson()
+					.get("http://api.lemonfree.com/v2/makes/?format=json&key="+getKey()).asJson()
 					.getBody();
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
@@ -26,7 +32,7 @@ public class LemonFreeService {
 		JsonNode responseModelsByMakeID = null;
 		try {
 			responseModelsByMakeID = Unirest
-					.get("http://api.lemonfree.com/v2/models/?make_id="+id+"&format=json&key=07859aa5804ac8c1425bdb703ca961ed")
+					.get("http://api.lemonfree.com/v2/models/?make_id="+id+"&format=json&key="+getKey())
 					.asJson().getBody();
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
@@ -35,6 +41,25 @@ public class LemonFreeService {
 
 		return responseModelsByMakeID;
 	}
+	
+	private static String getKey() {
+
+		String useKey = "";
+		int intMod = round % 2;
+
+		switch (intMod) {
+		case 0:
+			useKey = keyKuntal;
+			break;
+		case 1:
+			useKey = keyChitta;
+			break;
+		}
+
+		round++;
+		return useKey;
+	}
+	
 //
 //	public static JsonNode getTrimsByMakeandModel() {
 //
