60a6a069eeaa2897f917f032ae401c3e6d721ae2
vidushidikshit <vibhashiva92@gmail.com>
2016-04-24 23:32:39 -0700

    Test Cases.

--- src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
+++ src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
@@ -1,10 +1,19 @@
 package edu.csula.datascience.acquisition;
 
 import com.google.common.collect.Lists;
+
+import edu.csula.vkc.models.Make;
+import edu.csula.vkc.models.Model;
+import edu.csula.vkc.models.Price;
+import edu.csula.vkc.models.Styles;
+import edu.csula.vkc.models.Years;
+import edu.csula.vkc.util.DummySource;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,28 +23,27 @@ import static org.junit.Assert.*;
  * A test case to show how to use Collector and Source
  */
 public class CollectorTest {
-    private Collector<SimpleModel, MockData> collector;
-    private Source<MockData> source;
+	private Collector<SimpleModel, MockData> collector;
+	private Source<MockData> source;
 
-    @Before
-    public void setup() {
-        collector = new MockCollector();
-        source = new MockSource();
-    }
+	@Before
+	public void setup() {
+		collector = new MockCollector();
+		source = new MockSource();
+	}
 
-    @Test
-    public void mungee() throws Exception {
-        List<SimpleModel> list = (List<SimpleModel>) collector.mungee(source.next());
-        List<SimpleModel> expectedList = Lists.newArrayList(
-            new SimpleModel("2", "content2"),
-            new SimpleModel("3", "content3")
-        );
+	@Test
+	public void mungee() throws Exception {
+		List<SimpleModel> list = (List<SimpleModel>) collector.mungee(source.next());
+		List<SimpleModel> expectedList = Lists.newArrayList(new SimpleModel("2", "content2"),
+				new SimpleModel("3", "content3"));
 
-        Assert.assertEquals(list.size(), 2);
+		Assert.assertEquals(list.size(), 2);
 
-        for (int i = 0; i < 2; i ++) {
-            Assert.assertEquals(list.get(i).getId(), expectedList.get(i).getId());
-            Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
-        }
-    }
+		for (int i = 0; i < 2; i++) {
+			Assert.assertEquals(list.get(i).getId(), expectedList.get(i).getId());
+			Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
+		}
+	}
+
 }
