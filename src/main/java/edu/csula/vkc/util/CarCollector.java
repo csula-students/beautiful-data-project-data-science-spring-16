501795742fc351c16e7136b35977383bf828f28a
Chittaksh <khadse.chittaksh@gmail.com>
2016-05-23 15:20:13 -0700

    CarCollector created.

---
+++ src/main/java/edu/csula/vkc/util/CarCollector.java
@@ -0,0 +1,66 @@
+package edu.csula.vkc.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.bson.Document;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import edu.csula.datascience.acquisition.Collector;
+import edu.csula.vkc.models.Price;
+import edu.csula.vkc.models.Vehicle;
+
+public class CarCollector implements Collector<Vehicle, Vehicle> {
+
+	MongoClient mongoClient;
+	MongoDatabase database;
+	MongoCollection<Document> collection;
+
+	public CarCollector() {
+		super();
+	}
+
+	@Override
+	public Collection<Vehicle> mungee(Collection<Vehicle> makesList) {
+
+		List<Vehicle> makesdataListtoRemove = new ArrayList<Vehicle>();
+		List<Price> Price = new ArrayList<Price>();
+
+		for (Vehicle make : makesList) {
+
+			//Code to filter Vehicals.
+			
+		}
+		makesList.removeAll(makesdataListtoRemove);
+		System.out.println(makesList.size());
+
+		return makesList;
+	}
+
+	@Override
+	public void save(Collection<Vehicle> data) {
+		try {
+
+			// establish database connection to MongoDB
+			mongoClient = new MongoClient();
+
+			//Database Name.
+			database = mongoClient.getDatabase("bd-example");
+
+			//Collection Name.
+			collection = database.getCollection("test");
+
+			ObjectMapper mapper = new ObjectMapper();
+
+			for (Vehicle vehicle : data) {
+				collection
+						.insertOne(Document.parse(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(vehicle)));
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+}
