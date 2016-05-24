750e2efa850a7781e00950f614c6d01f9e0adb54
Chittaksh <khadse.chittaksh@gmail.com>
2016-05-23 18:57:38 -0700

    Update for new calls.

--- src/main/java/edu/csula/vkc/util/WriteToJson.java
+++ src/main/java/edu/csula/vkc/util/WriteToJson.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.csula.vkc.models.Make;
@@ -15,7 +13,7 @@ public class WriteToJson {
 
 	private static String location = "C:\\Users\\chitt_000\\Documents\\";
 	private static String folderForData = "data-science-2016\\";
-	private static String fileName = "name"+ (new Date()).toString() +".json";
+	//private static String fileName = "name"+ (new Date()).toString() +".json";
 
 	public boolean writeNewFile(String serviceName, Collection<Make> make) {
 
@@ -77,9 +75,9 @@ public class WriteToJson {
 		}
 	}
 
-	public boolean writeNewFile(Collection<Vehicle> vehical) {
+	public boolean writeNewVehicleFile(String makeName, String modelName, Vehicle vehicle) {
 		try {
-			File f = new File(location + folderForData + fileName);
+			File f = new File(location + folderForData + makeName + "." + modelName + ".json");
 
 			if (f.exists()) {
 				// Do Nothing
@@ -93,8 +91,8 @@ public class WriteToJson {
 
 			// System.out.println(make.get(0).getModelList().get(0).getModel().toString());
 
-			FileWriter file = new FileWriter(location + folderForData + fileName, true);
-			file.write(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(vehical));
+			FileWriter file = new FileWriter(location + folderForData + makeName + "." + modelName + ".json", true);
+			file.write(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(vehicle));
 			file.flush();
 			file.close();
 
