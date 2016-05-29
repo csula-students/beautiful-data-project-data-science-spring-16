d8c087855bf2b3ee06be9bee60e6644e8823b644
Chittaksh <khadse.chittaksh@gmail.com>
2016-05-29 11:41:34 -0700

    just updated one.

--- src/main/java/edu/csula/vkc/apps/TheApp.java
+++ src/main/java/edu/csula/vkc/apps/TheApp.java
@@ -1,8 +1,10 @@
 package edu.csula.vkc.apps;
 
 import java.util.Collection;
+import java.util.List;
 
-import edu.csula.datascience.acquisition.Collector;
+import com.google.common.collect.Lists;
+
 import edu.csula.vkc.models.Make;
 import edu.csula.vkc.util.CSVSource;
 import edu.csula.vkc.util.DummySource;
@@ -13,6 +15,8 @@ import edu.csula.vkc.util.LemonSource;
 public class TheApp {
 	public static void main(String[] args) throws Exception {
 
+		List<Make> listEdmundsMakes = Lists.newArrayList();
+
 		// Sources
 		EdmundsSource edmundsSource = new EdmundsSource();
 		LemonSource lemonSource = new LemonSource();
@@ -20,27 +24,32 @@ public class TheApp {
 
 		GenericCollector collector = new GenericCollector();
 
-		// Collection<Make> edmundsData = edmundsSource.getData();
-		// Collection<Make> cleanedEdmundsData = collector.mungee(edmundsData);
-		// collector.save(cleanedEdmundsData);
-		//
-		 Collection<Make> lemonData = lemonSource.getData();
-		 Collection<Make> cleanedLemonData = collector.mungee(lemonData);
-		 collector.save(cleanedLemonData);
+		listEdmundsMakes = edmundsSource.getMakes();
 		
+		if (edmundsSource.hasNext()) {
+			 Collection<Make> edmundsData = edmundsSource.getMakes();
+			 Collection<Make> cleanedEdmundsData =
+			 collector.mungee(edmundsData);
+			 collector.save(cleanedEdmundsData);
+		}
+
+		// Collection<Make> lemonData = lemonSource.getData();
+		// Collection<Make> cleanedLemonData = collector.mungee(lemonData);
+		// collector.save(cleanedLemonData);
+		//
 		// if(csvSource.hasNext()){
 		// Collection<Make> csvData = csvSource.next();
 		// Collection<Make> cleanedCSVData = collector.mungee(csvData);
 		// collector.save(cleanedCSVData);
 		// }
-
-		DummySource dummySource = new DummySource(Long.MAX_VALUE);
-
-		if (dummySource.hasNext()) {
-			Collection<Make> dummyData = dummySource.next();
-			Collection<Make> cleanedDummyData = collector.mungee(dummyData);
-			collector.save(cleanedDummyData);
-		}
+		//
+		// DummySource dummySource = new DummySource(Long.MAX_VALUE);
+		//
+		// if (dummySource.hasNext()) {
+		// Collection<Make> dummyData = dummySource.next();
+		// Collection<Make> cleanedDummyData = collector.mungee(dummyData);
+		// collector.save(cleanedDummyData);
+		// }
 
 	}
 }
