cc43a1c251df63614d181716c16809fae4386cbc
Chittaksh <khadse.chittaksh@gmail.com>
2016-05-29 11:39:42 -0700

    JSON to Elastic Search. working and final.

--- src/main/java/edu/csula/vkc/apps/ElasticSearch.java
+++ src/main/java/edu/csula/vkc/apps/ElasticSearch.java
@@ -2,15 +2,23 @@ package edu.csula.vkc.apps;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import org.apache.lucene.store.SleepingLockWrapper;
 import org.elasticsearch.action.bulk.*;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,11 +29,11 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 public class ElasticSearch {
 
 	private final static String indexName = "car";
-	private final static String typeName = "Vehicle";
+	private final static String typeName = "vehicle";
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
 		Node node = nodeBuilder()
-				.settings(Settings.builder().put("cluster.name", "chittaksh").put("path.home", "elasticsearch-data"))
+				.settings(Settings.builder().put("cluster.name", "elastic-quiz").put("path.home", "elasticsearch-data"))
 				.node();
 		Client client = node.client();
 
@@ -48,6 +56,7 @@ public class ElasticSearch {
 
 			@Override
 			public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+				System.out.println(executionId);
 			}
 
 			@Override
@@ -55,7 +64,7 @@ public class ElasticSearch {
 				System.out.println("Facing error while importing data to elastic search");
 				failure.printStackTrace();
 			}
-		}).setBulkActions(10000).setBulkSize(new ByteSizeValue(1, ByteSizeUnit.GB))
+		}).setBulkActions(5).setBulkSize(new ByteSizeValue(1, ByteSizeUnit.GB))
 				.setFlushInterval(TimeValue.timeValueSeconds(5)).setConcurrentRequests(1)
 				.setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3)).build();
 
@@ -63,9 +72,6 @@ public class ElasticSearch {
 		Gson gson = new Gson();
 
 		try {
-			// after reading the csv file, we will use CSVParser to parse
-			// through
-			// the csv files
 			JsonParser parser = new JsonParser();
 			Object obj = parser.parse(new FileReader(jsonFile));
 			JsonArray arrayFile = (JsonArray) obj;
@@ -73,18 +79,38 @@ public class ElasticSearch {
 			for(int i = 0; i <arrayFile.size(); i++){
 				System.out.println(arrayFile.get(i).toString()); // This statement is printing that..
 				
-				bulkProcessor.add(new IndexRequest(indexName, typeName)
-                        .source(gson.toJson(arrayFile.get(i)))
+				JsonObject objcurrent = (JsonObject) arrayFile.get(i);
+
+				bulkProcessor.add(new IndexRequest(indexName, typeName, objcurrent.get("id").getAsString())
+                        .source(arrayFile.get(i).toString())
                     );
 			}
-
 			// System.out.println(arrayFile.toString());
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		/**
 		 * Structured search
 		 */
+		
+		/**
+         * AGGREGATION
+         */
+//        SearchResponse sr = node.client().prepareSearch(indexName)
+//            .setTypes(typeName)
+//            .setQuery(QueryBuilders.matchAllQuery())
+//            .addAggregation(
+//                AggregationBuilders.terms("stateAgg").field("makeName")
+//                    .size(Integer.MAX_VALUE)
+//            )
+//            .execute().actionGet();
+//
+//        // Get your facet results
+//        Terms agg1 = sr.getAggregations().get("stateAgg");
+//
+//        for (Terms.Bucket bucket: agg1.getBuckets()) {
+//            System.out.println(bucket.getKey() + ": " + bucket.getDocCount());
+//        }
 	}
 }
