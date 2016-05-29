83b3a8a56ac57276920080dfec4954ac480d7bf6
Chittaksh <khadse.chittaksh@gmail.com>
2016-05-29 12:08:55 -0700

    Edmunds Testing app.

--- src/main/java/edu/csula/vkc/apps/EdmundsServiceApp.java
+++ src/main/java/edu/csula/vkc/apps/EdmundsServiceApp.java
@@ -12,19 +12,20 @@ public class EdmundsServiceApp {
 		System.out.println();
 		
 		
-		/*
+		
 		
 		System.out.println("getMakes response-->" + carMakes);
 		
-
-		JsonNode carModelsandYears = EdmundsService.getModelandYear();
+		/*
+		 * 
+		JsonNode carModelsandYears = EdmundsService.get;
 
 		System.out.println("getModelandYear response-->" + carModelsandYears);
 
 		JsonNode carStyleID = EdmundsService.getStyleID();
 
 		System.out.println("getStyleID response-->" + carStyleID);
-
+		
 		// JsonNode carTCOPrice = EdmundsService.getTCOPrice();
 
 		// System.out.println("Car TCO Price response-->" + carTCOPrice);
