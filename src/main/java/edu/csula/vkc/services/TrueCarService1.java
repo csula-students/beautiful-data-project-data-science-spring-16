decfbedccb71343355cde6cbc42e78509aa13d0b
vidushidikshit <vibhashiva92@gmail.com>
2016-06-08 16:55:16 -0700

    retrieving price

---
+++ src/main/java/edu/csula/vkc/services/TrueCarService1.java
@@ -0,0 +1,47 @@
+package edu.csula.vkc.services;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class TrueCarService1 {
+
+	public static int getListing(String makeName, String modelName) {
+
+		int price = 0;
+		try {
+
+			String urlString = "https://www.truecar.com/used-cars-for-sale/listings/"
+					+ makeName.replace(" ", "-").toLowerCase() + "/" + modelName.replace(" ", "-").toLowerCase() + "/";
+
+			// System.out.println(urlString);
+			Document doc = Jsoup.connect(urlString).timeout(7000).get();
+
+			Elements prices = doc.getElementsByClass("price");
+
+			for (Element element : prices) {
+					if(element.hasText())
+					System.out.println(element.text().replace("$", "").replace(",", "").trim());
+				}
+				
+			
+
+			// System.out.println(strName.text());
+			// if (strPrice.text().lastIndexOf("$") != -1) {
+			// price = Integer.parseInt(
+			// strPrice.text().substring(strPrice.text().lastIndexOf("$")).replace("$",
+			// "").replace(",", ""));
+			// }
+			// System.out.println(price);
+		} catch (
+
+		Exception e) {
+			System.out.print("Source : Microsoft Service");
+			System.out.println(e.toString());
+			return 0;
+		}
+		return price;
+	}
+
+}
