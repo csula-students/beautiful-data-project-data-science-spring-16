f51be14c468ece1c3b30b218cc50347979aa6f0c
Chittaksh <khadse.chittaksh@gmail.com>
2016-06-08 17:43:49 -0700

    Getting the Details.

--- src/main/java/edu/csula/vkc/services/TrueCarService.java
+++ src/main/java/edu/csula/vkc/services/TrueCarService.java
@@ -12,34 +12,64 @@ public class TrueCarService {
 		int price = 0;
 		try {
 
-			String urlString = "https://www.truecar.com/used-cars-for-sale/listings/"
+			String firstURL = "https://www.truecar.com/used-cars-for-sale/listings/"
 					+ makeName.replace(" ", "-").toLowerCase() + "/" + modelName.replace(" ", "-").toLowerCase() + "/";
 
 			// System.out.println(urlString);
-			Document doc = Jsoup.connect(urlString).timeout(7000).get();
+			Document docu = Jsoup.connect(firstURL).timeout(7000).get();
+			Elements pages = docu.getElementsByClass("pagination");
+			Elements links = pages.select("a[href]");
+			// System.out.println(links);
+			//System.out.println(links.last().attr("data-page-index"));
+			int noPages = Integer.parseInt(links.last().attr("data-page-index"));
 
-			Elements strName = doc.getElementsByClass("vdp-link");
+			for (int i = 1; i <= noPages; i++) {
 
-			for (Element element : strName) {
-				int a = 1;
-				if (element.hasText()&& !element.text().equals("View Details")) {
-					
-					System.out.println(element.text().substring(0, 4));
+				String urlString = "https://www.truecar.com/used-cars-for-sale/listings/"
+						+ makeName.replace(" ", "-").toLowerCase() + "/" + modelName.replace(" ", "-").toLowerCase()
+						+ "/?page="+i;
+
+				// System.out.println(urlString);
+				Document doc = Jsoup.connect(urlString).timeout(7000).get();
+
+				Elements strYear = doc.getElementsByClass("vdp-link");
+
+				for (Element element : strYear) {
+					if (element.hasText() && !element.text().equals("View Details")) {
+						// System.out.println(element.text().substring(0, 4));
+						element.text().substring(0, 4);
+					}
 				}
-				a++;
-			}
 
-			// System.out.println(strName.text());
-			// if (strPrice.text().lastIndexOf("$") != -1) {
-			// price = Integer.parseInt(
-			// strPrice.text().substring(strPrice.text().lastIndexOf("$")).replace("$",
-			// "").replace(",", ""));
-			// }
-			// System.out.println(price);
+				Elements strVehicleInfo = doc.getElementsByClass("vehicle-info");
+
+				// System.out.println(strVehicleInfo);
+				for (Element element : strVehicleInfo) {
+					if (element.text().contains("miles")) {
+						//System.out.println(element.text().substring(0, element.text().indexOf("miles")).replace("Mileage:", "").replace(",", "").trim());
+						element.text().substring(0, element.text().indexOf("miles")).replace("Mileage:", "")
+								.replace(",", "").trim();
+					}
+				}
+
+				Elements strPrices = doc.getElementsByClass("price");
+
+				for (Element element : strPrices) {
+					if (element.hasText())
+						// System.out.println(element.text().replace("$",
+						// "").replace(",", "").trim());
+						element.text().replace("$", "").replace(",", "").trim();
+				}
+				
+				System.out.println(strYear.size());
+				System.out.println(strPrices.size());
+				System.out.println(strVehicleInfo.size());
+
+			}
 		} catch (
 
 		Exception e) {
-			System.out.print("Source : Microsoft Service");
+			System.out.print("Source : TrueCar Service");
 			System.out.println(e.toString());
 			return 0;
 		}
