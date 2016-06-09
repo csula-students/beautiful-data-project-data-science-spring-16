13352844a9c24d335b7084a74552f1e5d01405ba
Chittaksh <khadse.chittaksh@gmail.com>
2016-06-09 16:54:12 -0700

    Final Application.

--- src/main/java/edu/csula/vkc/third/apps/TheOne.java
+++ src/main/java/edu/csula/vkc/third/apps/TheOne.java
@@ -1,10 +1,11 @@
 package edu.csula.vkc.third.apps;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import com.google.common.collect.Lists;
-
 import edu.csula.vkc.services.TrueCarService;
+import edu.csula.vkc.third.models.Details;
 import edu.csula.vkc.third.models.Vehicle;
 import edu.csula.vkc.third.util.GenericSource;
 import edu.csula.vkc.third.util.VehicleCollector;
@@ -13,20 +14,16 @@ public class TheOne {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		
 		List<Vehicle> listVehicals = Lists.newArrayList();
 
 		// Sources
 		GenericSource genericSource = new GenericSource();
 		VehicleCollector collector = new VehicleCollector();
 		
-		//Testing call to Microsoft page.
-		TrueCarService.getListing("Acura", "ILX");
-		
-//		while(genericSource.hasNext()){
-//			listVehicals = genericSource.next();
-//			Collection<Vehicle> cleanedVehicals = collector.mungee(listVehicals);
-//			collector.save(cleanedVehicals);
-//		}
+		while(genericSource.hasNext()){
+			listVehicals = genericSource.next();
+			Collection<Vehicle> cleanedVehicals = collector.mungee(listVehicals);
+			collector.save(cleanedVehicals);
+		}
 	}
 }
