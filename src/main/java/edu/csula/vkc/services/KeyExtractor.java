674a9d904aa050d7a691a03d88b54af87971fc19
vidushidikshit <vibhashiva92@gmail.com>
2016-06-09 13:36:31 -0700

    changes

--- src/main/java/edu/csula/vkc/services/KeyExtractor.java
+++ src/main/java/edu/csula/vkc/services/KeyExtractor.java
@@ -8,33 +8,33 @@ import java.util.Scanner;
 public class KeyExtractor {
 	static ArrayList<String> edmunds = new ArrayList<String>();
 	static ArrayList<String> lemonfree = new ArrayList<String>();
-	static boolean flag=false;
-	public static void keys() throws FileNotFoundException{
-		if(edmunds.size()==0){
-		Scanner s = new Scanner(new File("C:/Users/vidus/Desktop/Vidushi/spring 2016/cs594 Big data/keys.txt"));
-		while (s.hasNext()) {
-			
-			if(flag!=true)
-				if(s.hasNext("lemonfree"))
-				flag=true;
-			
-			if(flag==true)
-				lemonfree.add(s.next());
-			else
-				edmunds.add(s.next());
-				
-		}
-		
-		edmunds.remove(0);
-		lemonfree.remove(0);
-			
-		/*for (String edmund1 : edmunds) {
-			System.out.println(edmund1);
-		}
-		for (String lemonfree1 : lemonfree) {
-			System.out.println(lemonfree1);
-		}*/
-		s.close();
+	static boolean flag = false;
+
+	public static void keys() throws FileNotFoundException {
+		if (edmunds.size() == 0) {
+			Scanner s = new Scanner(new File("C:/Users/vidus/Desktop/Vidushi/spring 2016/cs594 Big data/keys.txt"));
+			while (s.hasNext()) {
+
+				if (flag != true)
+					if (s.hasNext("lemonfree"))
+						flag = true;
+
+				if (flag == true)
+					lemonfree.add(s.next());
+				else
+					edmunds.add(s.next());
+
+			}
+
+			edmunds.remove(0);
+			lemonfree.remove(0);
+
+			/*
+			 * for (String edmund1 : edmunds) { System.out.println(edmund1); }
+			 * for (String lemonfree1 : lemonfree) {
+			 * System.out.println(lemonfree1); }
+			 */
+			s.close();
 		}
 	}
 
@@ -43,16 +43,16 @@ public class KeyExtractor {
 
 	public String getEdmundsKey() throws FileNotFoundException {
 		keys();
-		indexEdmunds = indexEdmunds%edmunds.size();
+		indexEdmunds = indexEdmunds % edmunds.size();
 		return edmunds.get(indexEdmunds++);
 
 	}
-	
-	public  String getLemonfreeKey() throws FileNotFoundException{
+
+	public String getLemonfreeKey() throws FileNotFoundException {
 		keys();
-		indexLemonFree=indexLemonFree%lemonfree.size();
+		indexLemonFree = indexLemonFree % lemonfree.size();
 		return lemonfree.get(indexLemonFree++);
-		
+
 	}
 
 }
