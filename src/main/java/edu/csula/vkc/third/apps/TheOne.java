357af352287a74239e26b05c83fb2f243159c2f2
Chittaksh <khadse.chittaksh@gmail.com>
2016-06-09 22:35:42 -0700

    latest version.

--- src/main/java/edu/csula/vkc/third/apps/TheOne.java
+++ src/main/java/edu/csula/vkc/third/apps/TheOne.java
@@ -3,6 +3,8 @@ package edu.csula.vkc.third.apps;
 import java.util.Collection;
 import java.util.List;
 import com.google.common.collect.Lists;
+
+import edu.csula.vkc.services.TrueCarService1;
 import edu.csula.vkc.third.models.Vehicle;
 import edu.csula.vkc.third.util.GenericSource;
 import edu.csula.vkc.third.util.VehicleCollector;
@@ -17,6 +19,8 @@ public class TheOne {
 		VehicleCollector collector = new VehicleCollector();
 		
 		//MicrosoftService.getMinPrice("Acura", "ILX", "2014");
+		
+		//TrueCarService1.getListing("Acura", "ILX");
 		
 		while(genericSource.hasNext()){
 			listVehicals = genericSource.next();
